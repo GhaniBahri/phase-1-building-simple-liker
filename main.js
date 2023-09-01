714f46e630b6139513a695ad0c68d79f12b01b7c
GhaniBahri <bahri.ghanou@gmail.com>
2023-09-01 13:28:07 +0200

    Completed Assingment

--- main.js
+++ main.js
@@ -3,7 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+const errModal= document.querySelector('div#modal')
+const like = document.getElementsByClassName('like')
+Array.from(like).forEach(el=>{
+  el.addEventListener('click',(e)=>{
+    const heart= el.querySelector('span.like-glyph')
+    mimicServerCall().then(()=>{
+      if (heart.innerText === EMPTY_HEART){
+        heart.classList.add('activated-heart')
+        heart.innerText= FULL_HEART
+      } else{
+        heart.classList.remove('activated-heart')
+        heart.innerText= EMPTY_HEART
+      }
+    }).catch(err=>{
+      console.log("err",err)
+      errModal.querySelector('p#modal-message').innerText= err
+      errModal.classList.remove('hidden')
+      setTimeout(()=>errModal.classList.add('hidden') , 3000)
+    })
+    
+   
+  })
+})
 
 
 
